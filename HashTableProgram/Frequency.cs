ecc7c1f5d1f582a69061e892122e36a302a52aae
SumitomShome <sumitomshome@gmail.com>
2020-12-04 23:10:35 +0530

    [Sumitom] Add: Method to find frequency of word in a paragraph

--- HashTableProgram/Frequency.cs
+++ HashTableProgram/Frequency.cs
@@ -5,15 +5,15 @@ namespace HashTableProblem
 {
     public class Frequency
     {
-        Dictionary<string, int> freq = new Dictionary<string, int>();
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
         public void WordFrequency(MyMapNode<int, string> hash)
         {
             for (int key = 0; key < hash.size; key++)
             {
-                freq.TryAdd(hash.Get(key).ToLower(), 0);
-                freq[hash.Get(key).ToLower()]++;
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
             }
-            foreach (KeyValuePair<string, int> item in freq)
+            foreach (KeyValuePair<string, int> item in frequency)
             {
                 Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
             }
